da90f1e3ced8bcbc1c58adf7b65181e9f3ed5bd5
Eleftherios Chetzakis <eleftherios@workable.com>
2021-11-24 19:00:18 +0200

    Add fastify routes

--- fastify.js
+++ fastify.js
@@ -1,18 +1,45 @@
 // CommonJs
 const fastify = require('fastify')({
-  logger: false
+    logger: false
 });
+
+const service = require('./person-service');
+const resource = 'persons';
+
+// Get
+fastify.get(`/${resource}/:id`, async (request, reply) => {
+    const person = await service.find(request.params.id);
+    person ? reply.send(person) : reply.code(404).send();
+});
+
+// List
+fastify.get(`/${resource}`, async (request, reply) => {
+    const persons = await service.list();
+    reply.send(persons);
+});
+
+// Create
+fastify.post(`/${resource}`, async (request, reply) => {
+    const person = await service.add(request.body);
+    reply.code(201);
+    reply.send(person);
+});
+
+//Update
+fastify.patch(`/${resource}/:id`, async (request, reply) => {
+    const person = await service.find(request.params.id);
+    if (!person) {
+        reply.code(404).send();
+        return;
+    }
+    const updated = await service.update({ id: request.params.id, ...request.body });
+    reply.send(updated);
+});
+
 
 // Run the server!
 fastify.listen(3001, (err, address) => {
-  console.log('fastify server listening in 3001');
-  if (err) throw err;
-  // Server is now listening on ${address}
+    console.log('fastify server listening in 3001');
+    if (err) throw err;
+    // Server is now listening on ${address}
 });
-
-// implementation
-
-// Declare a route
-// fastify.get('/', (request, reply) => {
-//     reply.send({ hello: 'world' });
-//   });
